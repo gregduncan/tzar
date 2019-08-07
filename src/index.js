6d3d178355525c8fc0487ac881067d9aa88872df
Greg Duncan <mail@gregduncan.co.uk>
2019-08-07 09:31:48 +0100

    Added react router dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import store from './store';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route exact path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
