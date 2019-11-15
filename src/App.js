63c9a3b31c661f94cdba31ff20949edd0ab5a371
Greg Duncan <mail@gregduncan.co.uk>
2019-11-15 14:21:31 +0000

    Init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 
-import payload from './store/data/form.json';
+import payload from './store/data/benchmark-r3-d0.json';
 import { Form, JsonView } from './components';
 
 function App() {
